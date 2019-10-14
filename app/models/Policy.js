68dfe62c477cd4a2a639f7fc90384f1422e9d64d
Albert Morote <albert-morote2@hotmail.com>
2019-10-14 13:48:07 +0200

    Add Client and policy models

---
+++ app/models/Policy.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const PolicySchema = mongoose.Schema({
+	id: {
+		type: String,
+		required: true
+	},
+	amountInsured: {
+		type: Number,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	inceptionDate: {
+		type: Date,
+		required: true
+	},
+	installmentPayment: {
+		type: Boolean,
+		required: true
+	},
+	clientId: {
+		type: String,
+		required: true
+	}
+});
+
+module.exports = mongoose.model('Policies', PolicySchema);
--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const https = require('http');
 require ('dotenv/config');
 
-//Import router
-const clientsRoute = require('./routes/clients')
+//Import Client mongoose schema
+const Client = require('./app/models/Client');
+const Policy = require('./app/models/Policy');
 
+//Import router
+const clientsRoute = require('./app/routes/clients')
+const policiesRoute = require('./app/routes/policies')
+
+//Middleware
 app.use('/api/clients', clientsRoute);
+app.use('/api/policies', policiesRoute);
+
+
+//Connect to MongoDB
 mongoose.connect(
 	process.env.DB_CONNECTION,
 	{ useNewUrlParser: true, useUnifiedTopology: true },
-	() => console.log('Connected to DB')
+	(err, client) => {
+		if(err) {
+        	console.log(err)
+    	} else {
+    	console.log('Connected to DB')
+    	}
+	}
 );
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on ${port}`))
