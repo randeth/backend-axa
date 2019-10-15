ca375d11740979099f702df29396819676e2420d
Albert Morote <albert-morote2@hotmail.com>
2019-10-15 11:53:23 +0200

    Added endpoint to find the policies attached to a client

--- app/routes/login.js
+++ app/routes/login.js
@@ -34,9 +34,7 @@ router.post('/', async (req, res) => {
 function verifyToken (req, res, next) {
   const bearerHeader = req.headers.authorization
   if (bearerHeader !== undefined) {
-    const bearer = bearerHeader.split(' ')
-    const bearerToken = bearer[1]
-    req.token = bearerToken
+    req.token = bearerHeader.split(' ')[1]
     next()
   } else {
     res.sendStatus(403)
